ce1fa1db96baac81f77987b60853ee81191b05bd
postforty <75767073+postforty@users.noreply.github.com>
2023-04-30 23:38:57 +0900

    Update 과일 장수.js

--- solve/0506_과일장수/postforty/과일 장수.js
+++ solve/0506_과일장수/postforty/과일 장수.js
@@ -11,5 +11,5 @@ function solution(k, m, score) {
   return answer;
 }
 
-// console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
+console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
 console.log(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]));

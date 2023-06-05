0ed0414454105f74364325457ab69af7057f3718
Dandy Code! <postforty@naver.com>
2023-06-05 12:55:02 +0900

    Delete 성격 유형 검사하기.py

--- solve/0610_성격 유형 검사하기/postforty/성격 유형 검사하기.py
+++
@@ -1,6 +0,0 @@
-def solution(survey, choices):
-    answer = ''
-    return answer
-
-print(solution(["AN", "CF", "MJ", "RT", "NA"],	[5, 3, 2, 7, 5])) # "TCMA"
-print(solution(["TR", "RT", "TR"], [7, 1, 3])) # "RCJA"

2a4b6c23956ccca737088d9df1b6c3815d9345e9
Sungjae.Kwak <sungjae8970@gmail.com>
2023-06-18 20:22:58 +0900

    곽성재 - 주석 추가

--- solve/0617_신고 결과 받기/ssssssjay/신고결과받기.js
+++ solve/0617_신고 결과 받기/ssssssjay/신고결과받기.js
@@ -1,13 +1,17 @@
 function solution(id_list, report, k) {
   const answer = Array(id_list.length).fill(0);
+  // key: 신고받은 사람(string), value: 해당 유저를 신고한 사람들(Set)
   const BOARD = {};
+  // 보드 초기화
   id_list.forEach((id) => {
     BOARD[id] = new Set();
   });
+  // 신고건수들을 순회하며, 신고한 사람들(Set)에다가 추가
   report.forEach((str) => {
     const [report, reported] = str.split(" ");
     BOARD[reported].add(report);
   });
+  // 신고한 사람들의 길이가 k이상이면 그 신고자들에게 알림 건수를 1씩 증가한다
   for (const reporters of Object.values(BOARD)) {
     if (reporters.size >= k) {
       reporters.forEach((reporter) => {

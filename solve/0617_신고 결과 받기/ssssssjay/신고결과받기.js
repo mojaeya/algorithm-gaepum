31a79aa9c8cfad4f1683c8076d4bb0bc5d6f5688
Sungjae.Kwak <sungjae8970@gmail.com>
2023-06-18 19:54:38 +0900

    ✨ 곽성재 - 신고결과받기

--- solve/0617_신고 결과 받기/ssssssjay/신고결과받기.js
+++ solve/0617_신고 결과 받기/ssssssjay/신고결과받기.js
@@ -1,1 +1,20 @@
-function solution() {}
+function solution(id_list, report, k) {
+  const answer = Array(id_list.length).fill(0);
+  const BOARD = {};
+  id_list.forEach((id) => {
+    BOARD[id] = new Set();
+  });
+  report.forEach((str) => {
+    const [report, reported] = str.split(" ");
+    BOARD[reported].add(report);
+  });
+  for (const reporters of Object.values(BOARD)) {
+    if (reporters.size >= k) {
+      reporters.forEach((reporter) => {
+        answer[id_list.indexOf(reporter)] += 1;
+      });
+    }
+  }
+
+  return answer;
+}

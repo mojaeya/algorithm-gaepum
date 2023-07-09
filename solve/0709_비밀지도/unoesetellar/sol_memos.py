6a49752b14472f991bcb87a8b8c5f0865d237adb
inDev <unoestellar@naver.com>
2023-07-09 19:26:52 +0900

    solve - unoestellar

--- solve/0709_비밀지도/unoesetellar/sol_memos.py
+++ solve/0709_비밀지도/unoesetellar/sol_memos.py
@@ -2,10 +2,12 @@ def solution(n, arr1, arr2):
     
     # n 기준으로 arr1 , arr2 순회 . 
     # 결과물은 n 개짜리의  ,  '#' 또는 ' ' 으로 구성된 길이5의 문자열로 구성된 배열이어야 함. 
-    tmp = []    
+    tmp = []
+    
     #  arr1 해독하기     
     for i,v in enumerate(arr1):
         str1 = ''
+        print(i , v )
         for j in range(n):
             
             print(n - j - 1)
@@ -14,24 +16,37 @@ def solution(n, arr1, arr2):
                 v = v - 2 ** (n - j - 1)
             else:
                 str1 = str1 + ' '
+            
+        print(str1)
         tmp.append(str1)
+    
+    print('------arr1---')
+    print(tmp)
     # arr2 돌면서 최종 answer 구성하기 
     for i,v in enumerate(arr2):
         ansStr = ''
         str2 = ''
+        print(i , v )
         for j in range(n):
+            
+            print(n - j - 1)
             if int(v / 2 ** (n - j - 1) ) > 0:
                 str2 = str2 + '#'
                 v = v - 2 ** (n - j - 1)
             else:
                 str2 = str2 + ' '
+            
+        print(str2)
         
-        # 지도 2개 결합 로직 
         for index in range ( len ( tmp[i] ) ):
+            #print ('CHECK')
+            #print ( tmp[i][index])
             if str2[index] == '#' or  tmp[i][index] == '#' :
                 ansStr = ansStr  + '#'
             else:
                 ansStr = ansStr + ' '
-        # 결합 후 index 에 할당 
+        #print('-----------CHEKC ANSSTR -------')
+        #print(ansStr)
         tmp[i] = ansStr
-    return tmp
+    answer = tmp
+    return answer

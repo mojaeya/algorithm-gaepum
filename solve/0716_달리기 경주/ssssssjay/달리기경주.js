ee5ef593d3ac6bd33197a485f3969dd01e2153a6
Sungjae.Kwak <sungjae8970@gmail.com>
2023-07-16 20:01:17 +0900

    ✨ 달리기 경주 - 곽성재

---
+++ solve/0716_달리기 경주/ssssssjay/달리기경주.js
@@ -0,0 +1,29 @@
+function solution(players, callings) {
+  const rankingName = {}; // 키: 등수, 밸류: 이름
+  const nameRanking = {}; // 키: 이름, 밸류: 등수
+  players.forEach((name, idx) => {
+    rankingName[idx] = name;
+    nameRanking[name] = idx;
+  });
+  callings.forEach((overtaker) => {
+    const curIdx = nameRanking[overtaker]; // 추월할 자의 현재 등숟
+    const nextIdx = curIdx - 1; // 추월하고 나서의 등수(한명 제치므로 마이너스 1)
+    const loser = rankingName[nextIdx]; // 추월당하는 사람의 이름
+
+    // 순위 뒤집어주기(추월자는 -1, 추월당한자는 +1)
+    // 두 개의 객체 모두를 변경한다
+    nameRanking[overtaker] = nextIdx;
+    nameRanking[loser] = curIdx;
+    rankingName[nextIdx] = overtaker;
+    rankingName[curIdx] = loser;
+  });
+
+  // 미리 길이만큼 배열선언을 해주지 않아도 자바스크립트의 특성 상 작동한다
+  const result = Array(players.length);
+
+  // 인덱스 차례대로 배열에 이름이 할당된다(0부터 담기는 것을 보장하지 않음!)
+  for (const [name, idx] of Object.entries(nameRanking)) {
+    result[idx] = name;
+  }
+  return result;
+}

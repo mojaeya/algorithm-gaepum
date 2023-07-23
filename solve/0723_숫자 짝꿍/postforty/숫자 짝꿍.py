b21c854eeac660061b032af9ca150c87c182c7f6
postforty <75767073+postforty@users.noreply.github.com>
2023-07-23 21:20:21 +0900

    Update 숫자 짝꿍.py

--- solve/0723_숫자 짝꿍/postforty/숫자 짝꿍.py
+++ solve/0723_숫자 짝꿍/postforty/숫자 짝꿍.py
@@ -84,10 +84,12 @@ def solution(X, Y):
     X_dict = {key: X.count(key) for key in set(X)}  # X의 각 요소의 개수를 카운트한 딕셔너리 생성
     Y_dict = {key: Y.count(key) for key in set(Y)}  # Y의 각 요소의 개수를 카운트한 딕셔너리 생성
 
+    print(X.count('5'))
+
     answer = ""
     for key in set(X) & set(Y):  # X와 Y에 공통으로 있는 요소만 처리
         count = min(X_dict[key], Y_dict[key])  # X와 Y에서 해당 요소의 최소 개수를 구함
-        print(count)
+        # print(count)
         answer += key * count  # 요소를 해당 개수만큼 반복하여 추가
 
     if not answer:
@@ -101,6 +103,6 @@ def solution(X, Y):
     return answer
 
 
-# print(solution("5525", "1255"))  # "552"
+print(solution("5525", "1255"))  # "552"
 # print(solution("100", "2345"))  # "-1"
-print(solution("100", "203045"))  # 	"0"
+# print(solution("100", "203045"))  # 	"0"

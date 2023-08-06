913cb677110eee855000a7ec21cbdd846aa2ac10
Sungjae.Kwak <sungjae8970@gmail.com>
2023-08-06 20:04:14 +0900

    완주하지못한선수 - 곽성재

--- solve/0716_달리기 경주/ssssssjay/달리기경주.js
+++ solve/0716_달리기 경주/ssssssjay/달리기경주.js
@@ -10,12 +10,10 @@ function solution(players, callings) {
     const nextIdx = curIdx - 1; // 추월하고 나서의 등수(한명 제치므로 마이너스 1)
     const loser = rankingName[nextIdx]; // 추월당하는 사람의 이름
 
-    // 순위 뒤집어주기(추월자는 -1, 추월당한자는 +1)
-    // 두 개의 객체 모두를 변경한다
-    nameRanking[overtaker] = nextIdx;
-    nameRanking[loser] = curIdx;
-    rankingName[nextIdx] = overtaker;
-    rankingName[curIdx] = loser;
+    // prettier-ignore
+    [nameRanking[overtaker], nameRanking[loser]] = [nameRanking[loser], nameRanking[overtaker]];
+    // prettier-ignore
+    [rankingName[curIdx], rankingName[nextIdx]] = [rankingName[nextIdx], rankingName[curIdx]];
   });
 
   // 미리 길이만큼 배열선언을 해주지 않아도 자바스크립트의 특성 상 작동한다
